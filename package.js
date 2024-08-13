d49967009cab5cf9adb26254d0774c667e20673c
graemepyle <graeme@graemep.me>
2024-08-13 14:43:11 +0400

    Change various to get successful build for Meteor 2.16 & Android 12

--- package.js
+++ package.js
@@ -5,15 +5,15 @@ Package.describe({
   git: 'https://github.com/quavedev/reloader/',
 });
 
-Cordova.depends({
-  'cordova-plugin-splashscreen': '5.0.3',
-});
+// Cordova.depends({
+//   'cordova-plugin-splashscreen': '5.0.3',
+// });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.10.2');
 
   api.use(
-    ['ecmascript', 'reload', 'reactive-var', 'tracker', 'launch-screen'],
+    ['ecmascript', 'reload', 'reactive-var', 'tracker', 'launch-screen@2.0.0'],
     'client'
   );
 
