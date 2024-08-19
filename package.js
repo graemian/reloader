f49e344864bb494f137f6d6471b4ff9ebd97057a
graemepyle <graeme@graemep.me>
2024-08-19 22:06:23 +0400

    Bump ver

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'graemian:reloader',
-  version: '2.0.5',
+  version: '2.0.6',
   summary: 'More control over hot code push reloading',
   git: 'https://github.com/quavedev/reloader/',
 });
